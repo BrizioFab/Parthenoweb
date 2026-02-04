c0a5fe737c4fb40951a283a4513ea4f94a144cf0
TrIckSt33r <antonioincoronato03@gmail.com>
2026-02-04 13:40:04 +0100

    fix 4.0

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
+from mangum import Mangum
+from fast_api.app import app
+
+
+
+
+
+
 def handler(request):
     return {
         "statusCode": 200,
         "body": "Python runtime OK 🚀"
     }
 
+
+handler = Mangum(app)
